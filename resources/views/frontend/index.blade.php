492ad16e77f5619b3e6fcc3b5fe62ced6888d0e8
HabibTanoy <hbtonoy42@gmail.com>
2022-10-05 21:43:29 +0600

    team member image height fixed

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -313,13 +313,7 @@
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="{{URL::asset($employee->image) }}" alt="Image">
-                            {{--                            <div class="team-social">--}}
-                            {{--                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>--}}
-                            {{--                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>--}}
-                            {{--                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>--}}
-                            {{--                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>--}}
-                            {{--                            </div>--}}
+                            <img class="w-100" src="{{URL::asset($employee->image) }}" height="350px" alt="Image">
                         </div>
                         <div class="text-center py-4">
                             <h4 class="text-primary">{{$employee->name}}</h4>
